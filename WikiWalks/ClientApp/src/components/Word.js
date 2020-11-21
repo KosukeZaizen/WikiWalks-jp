f6106d5b1154a77b6930727e9676042cf1bfa965
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2020-11-21 17:33:16 +0900

    目次下のスペースの調整

--- WikiWalks/ClientApp/src/components/Word.js
+++ WikiWalks/ClientApp/src/components/Word.js
@@ -172,13 +172,13 @@ class PagesForTheTitles extends Component {
                     <br />
                     {lineChangeDesc}
                     <span id="indexOfVocabLists"></span>
-                    <div style={isWide ? { display: "flex" } : {}}>
+                    <div style={isWide ? { display: "flex", marginBottom: 20 } : {}}>
                         {categories && categories.length > 0 && (
                             <div
                                 style={{
                                     maxWidth: 500,
                                     padding: 10,
-                                    marginBottom: isWide ? 20 : 30,
+                                    marginBottom: isWide ? "auto" : 30,
                                     marginRight: isWide ? 20 : 0,
                                     border: "5px double gray",
                                     width: "100%",
