8380ab5f76f99ade978ac13c7de521ca47ea96a8
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2021-03-05 09:39:01 +0900

    自動整形

--- WikiWalks/Models/DBCon.cs
+++ WikiWalks/Models/DBCon.cs
@@ -4,28 +4,21 @@ using System.Data.SqlClient;
 using RelatedPages.Util;
 using System.Data;
 
-namespace RelatedPages.Models
-{
-    public class DBCon
-    {
+namespace RelatedPages.Models {
+    public class DBCon {
         public List<Dictionary<string, Object>> ExecuteSelect(
-            string sql, 
-            Dictionary<string, object[]> dicParams = null, 
+            string sql,
+            Dictionary<string, object[]> dicParams = null,
             int timeout = 60 * 60 * 2
-            )
-        {
+            ) {
             using (var connection = new SqlConnection(PrivateConsts.CONNECTION_STRING))
-            using (var command = new SqlCommand(sql, connection))
-            {
+            using (var command = new SqlCommand(sql, connection)) {
                 //参考：https://netsystem.jpn.org/t_nary/vb-net/sql-server-%E3%82%BF%E3%82%A4%E3%83%A0%E3%82%A2%E3%82%A6%E3%83%88%E9%96%A2%E9%80%A3%E3%81%AE%E8%A8%AD%E5%AE%9A/
                 command.CommandTimeout = timeout; //コマンド実行タイムアウト（デフォルト２時間とする）
-                try
-                {
+                try {
                     // パラーメータの置換
-                    if (dicParams != null)
-                    {
-                        foreach (KeyValuePair<string, object[]> kvp in dicParams)
-                        {
+                    if (dicParams != null) {
+                        foreach (KeyValuePair<string, object[]> kvp in dicParams) {
                             var param = command.CreateParameter();
                             param.ParameterName = kvp.Key;
                             param.SqlDbType = (SqlDbType)kvp.Value[0];
@@ -44,25 +37,19 @@ namespace RelatedPages.Models
 
                     var records = new List<Dictionary<string, Object>>();
 
-                    while (sdr.Read() == true)
-                    {
+                    while (sdr.Read() == true) {
                         var record = new Dictionary<string, Object>();
-                        for (int i = 0; i < sdr.FieldCount; i++)
-                        {
+                        for (int i = 0; i < sdr.FieldCount; i++) {
                             var value = sdr.GetValue(i);
                             record.Add(sdr.GetName(i), DBNull.Value.Equals(value) ? null : value);
                         }
                         records.Add(record);
                     }
                     return records;
-                }
-                catch (Exception exception)
-                {
+                } catch (Exception exception) {
                     Console.WriteLine(exception.Message);
                     throw exception;
-                }
-                finally
-                {
+                } finally {
                     // データベースの接続終了
                     connection.Close();
                 }
@@ -73,22 +60,18 @@ namespace RelatedPages.Models
             string sql,
             Dictionary<string, object[]> dicParams = null,
             int timeout = 0
-            )
-        {
+            ) {
             using (var connection = new SqlConnection(PrivateConsts.CONNECTION_STRING))
-            using (var command = new SqlCommand("SET ANSI_WARNINGS OFF; " + sql, connection))
-            {
+            using (var command = new SqlCommand("SET ANSI_WARNINGS OFF; " + sql, connection)) {
+
                 if (timeout != 0) {
                     //参考：https://netsystem.jpn.org/t_nary/vb-net/sql-server-%E3%82%BF%E3%82%A4%E3%83%A0%E3%82%A2%E3%82%A6%E3%83%88%E9%96%A2%E9%80%A3%E3%81%AE%E8%A8%AD%E5%AE%9A/
                     command.CommandTimeout = timeout; //コマンド実行タイムアウト
                 }
-                try
-                {
+                try {
                     // パラーメータの置換
-                    if (dicParams != null)
-                    {
-                        foreach (KeyValuePair<string, object[]> kvp in dicParams)
-                        {
+                    if (dicParams != null) {
+                        foreach (KeyValuePair<string, object[]> kvp in dicParams) {
                             var param = command.CreateParameter();
                             param.ParameterName = kvp.Key;
                             param.SqlDbType = (SqlDbType)kvp.Value[0];
@@ -105,14 +88,10 @@ namespace RelatedPages.Models
                     // SQLの実行
                     int result = command.ExecuteNonQuery();
                     return result >= 0;
-                }
-                catch (Exception exception)
-                {
+                } catch (Exception exception) {
                     Console.WriteLine(exception.Message);
                     throw exception;
-                }
-                finally
-                {
+                } finally {
                     // データベースの接続終了
                     connection.Close();
                 }
